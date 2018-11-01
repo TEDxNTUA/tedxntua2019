184cf3c44cfecb791cc011d7585c2294e7e0255a
Theo Diamantidis <diamaltho@gmail.com>
2018-11-01 20:43:51 +0200

    Change double to single quotes in urls.py docstring

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""project URL Configuration
+'''project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -12,7 +12,7 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+'''
 from django.contrib import admin
 from django.urls import path
 
