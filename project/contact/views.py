f867e3177e8b3ca1f31355293607d8d0dee07ffe
Giannis Daras <daras.giannhs@gmail.com>
2019-01-11 15:59:08 +0200

    Add demonstration of static content translation

--- project/contact/views.py
+++ project/contact/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.translation import gettext_lazy
+
 
 def index(request):
-    return HttpResponse('Hello.')
+    output = gettext_lazy("test_message")
+    return HttpResponse(output)
