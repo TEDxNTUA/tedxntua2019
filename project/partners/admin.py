f716fc5adea6488cde644ef13f78d3a866531d42
Giannis Daras <daras.giannhs@gmail.com>
2019-01-11 18:57:50 +0200

    Add a first version of partners admin, with internalization feature

--- project/partners/admin.py
+++ project/partners/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-
+from django import forms
 from .models import Partner
+import i18nfield.forms as i18nforms
 
-admin.site.register(Partner)
+class PartnerForm(forms.ModelForm):
+    def __str__(self):
+        return "partner_form"
+    class Meta:
+        model = Partner
+        fields = ['name', 'partner_type', 'link']
+        widgets = {
+            'name': i18nforms.I18nTextInput,
+        }
+
+class PartnerAdmin(admin.ModelAdmin):
+    form = PartnerForm
+
+
+admin.site.register(Partner, PartnerAdmin)
