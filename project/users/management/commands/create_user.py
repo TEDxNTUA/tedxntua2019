eb5a13df59f84512b6c20966c722cfe2930982e5
Giannis Daras <daras.giannhs@gmail.com>
2019-02-07 18:33:34 +0200

    Add management command for creating user that belongs to specific group

---
+++ project/users/management/commands/create_user.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+
+class Command(BaseCommand):
+    help = 'Create user.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('name', type=str)
+        parser.add_argument('email', type=str)
+        parser.add_argument('password', type=str)
+        parser.add_argument('team', type=str)
+
+    def handle(self, *args, **kwargs):
+        name = kwargs['name']
+        email = kwargs['email']
+        password = kwargs['password']
+        team = kwargs['team']
+        if (team == 'it'):
+            User.objects.create_superuser(name, email, password)
+        else:
+            user = User.objects.create_user(name, email, password)
+            user.is_staff = True
+            user.save()
+            group = Group.objects.get(name=team)
+            group.user_set.add(user)
