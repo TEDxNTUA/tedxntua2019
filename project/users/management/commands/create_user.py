d6f6b9bacc2c09ed0b08b46af708a3f233606b35
Giannis Daras <daras.giannhs@gmail.com>
2019-02-08 03:11:35 +0200

    Read password from input to avoid it staying in system memory

--- project/users/management/commands/create_user.py
+++ project/users/management/commands/create_user.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
+import getpass
+
 
 class Command(BaseCommand):
     help = 'Create user.'
@@ -8,14 +10,13 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('name', type=str)
         parser.add_argument('email', type=str)
-        parser.add_argument('password', type=str)
         parser.add_argument('team', type=str)
 
     def handle(self, *args, **kwargs):
         name = kwargs['name']
         email = kwargs['email']
-        password = kwargs['password']
         team = kwargs['team']
+        password = getpass.getpass()
         if (team == 'it'):
             User.objects.create_superuser(name, email, password)
         else:
