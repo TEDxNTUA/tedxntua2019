6d27676a0a4997aaed278fe40a189bfac9d883db
Theo Diamantidis <diamaltho@gmail.com>
2019-02-10 00:43:16 +0100

    Demo pages for About, Schedule, Speakers

--- project/about/views.py
+++ project/about/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class AboutView(View):
+    template_name = 'about/index.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {})
