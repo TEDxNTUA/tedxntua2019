abd61e60c87c960a6855797b8e7f86fa3b184564
Giannis Daras <daras.giannhs@gmail.com>
2019-02-15 19:51:12 +0200

    Pep8 fix to Partner view

--- project/partners/views.py
+++ project/partners/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from .models import Partner
 
-class PartnersView(View):
 
+class PartnersView(View):
     template = 'partners/index.html'
 
     def get(self, request, *args, **kwargs):
