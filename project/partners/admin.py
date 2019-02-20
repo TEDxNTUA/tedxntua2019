8f6f67b10ff4d3d8f9643b26625a6307f1efccb4
Giannis Daras <daras.giannhs@gmail.com>
2019-02-20 13:08:29 +0200

    Simplify the multilingual admin

--- project/partners/admin.py
+++ project/partners/admin.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
 from .models import Partner
-from parler.forms import TranslatableModelForm
 from parler.admin import TranslatableAdmin
 
-
-class PartnerAdminForm(TranslatableModelForm):
-    pass
-
-
-class PartnerAdmin(TranslatableAdmin):
-    form = PartnerAdminForm
-
-
-admin.site.register(Partner, PartnerAdmin)
+admin.site.register(Partner, TranslatableAdmin)
