25d48091c2e5fdc8760d11cf289b4270c62acc90
Giannis Daras <daras.giannhs@gmail.com>
2019-03-02 22:14:55 +0200

    Add initial speakers animation and setup speakers html, css

---
+++ project/program/assets/js/speakers.js
@@ -0,0 +1,24 @@
+var get_cover = function(elem){
+  for (let el of elem.childNodes){
+    if (el.className == "fill cover-img"){
+      return el
+    }
+  }
+}
+var mouse_monitor = function(e, elem) {
+   let x = e.clientX/innerWidth;
+   let y = e.clientY/innerHeight;
+   let move_x = (x>0.5) ? '-30px' : '30px';
+   let move_y = (y>0.5) ? '-20px' : '20px';
+   elem.style.setProperty("--translate-x", move_x);
+   elem.style.setProperty("--translate-y", move_y);
+}
+
+
+
+let elements =  document.getElementsByClassName("card");
+for (let elem of elements){
+  elem.addEventListener("mousemove", function(e){
+    mouse_monitor(e, elem)
+  })
+}
