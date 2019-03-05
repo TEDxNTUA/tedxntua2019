5225614dc674c58d91afe252814c70c7034950ab
Panagiotou <manopilipana97@gmail.com>
2019-03-05 17:10:18 +0200

    Translated schedule fields

--- project/program/schedule_urls.py
+++ project/program/schedule_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ScheduleView
+from .views import ScheduleView, set_language
 
 
 urlpatterns = [
     path('', ScheduleView.as_view(), name='schedule'),
+    path('language', set_language, name="language")
 ]
