2363f12b8ecaff6edf28f86776554cf979b61842
Panagiotou <manopilipana97@gmail.com>
2019-03-07 17:17:40 +0200

    Updates on pipes

--- project/home/assets/js/pipes.js
+++ project/home/assets/js/pipes.js
@@ -1,41 +1,45 @@
   //These change the output result
-  const seconds = 8;  //Total duration of the animation
+  const seconds = 5;  //Total duration of the animation
   const transition = "linear"; // Tranisiton animation of each path (not total)
   const max_lines = 50;
   const widthPercentage = 0.2 // Width percentage on screen
   const heightPercentage = 0.8 // Height percentage on screen
   const colors = [{'b': '#c1e3d2', 'f': '#323232'}];
   //const scale = 1/1.2 // The bigger the denominator, the bigger the svg
-  const actual_margin_left = 12; // Affects first item of viebox
-  // end
+  const actual_margin_left = 0.07; // Affects first item of viebox
+  // endz
 
   var randColor = colors[Math.floor(Math.random() * colors.length)];
   var background_color = window.getComputedStyle(document.body)["background-color"];
   var _ = require('underscore');
-  var svg = document.querySelector('#svgpipes');
-  var w = screen.width;
-  var h = screen.height;
-  svg.setAttribute('width', widthPercentage*w);
-  svg.setAttribute('height', heightPercentage*h);
-  svg.setAttribute("transform", "scale(1.0)");
-  vbox = svg.getAttribute('viewBox').trim().split(" ");
-  vbox[0] = actual_margin_left;
-  newbox = vbox.join(" ");
-  svg.setAttribute('viewBox', newbox);
-  svg.style.marginLeft = 0;
-  //Listen for screen changes
-  window.addEventListener('resize', function(event){
-    headertop = document.getElementById('header').clientHeight;
-    var w = screen.width;
-    var h = screen.height;
-    svg.setAttribute('width', widthPercentage*w);
-    svg.setAttribute('height', heightPercentage*h);
-    svg.setAttribute("transform", "scale(1.0)");
-    svg.style.marginLeft = 0;
+  var svgs = document.querySelectorAll('svg');
+  original_vbox = svgs[0].getAttribute('viewBox').trim().split(" ");
+  for(i=0; i<svgs.length; i++){
+    var svg = svgs[i];
+    var w = svg.parentElement.clientWidth;
+    var h = svg.parentElement.clientHeight;
+    svg.setAttribute('width', w);
+    svg.setAttribute('height', h);
+    svg.setAttribute('preserveAspectRatio', 'none');
     vbox = svg.getAttribute('viewBox').trim().split(" ");
-    vbox[0] = actual_margin_left;
     newbox = vbox.join(" ");
     svg.setAttribute('viewBox', newbox);
+    svg.style.marginLeft = 0;
+  }
+  window.addEventListener('resize', function(event){
+   var svgs = document.querySelectorAll('svg');
+   for(q=0; q<svgs.length; q++){
+     svg = svgs[q];
+     var w = svg.parentElement.clientWidth;
+     var h = svg.parentElement.clientHeight;
+     svg.setAttribute('width', w);
+     svg.setAttribute('height', h);
+     svg.setAttribute('preserveAspectRatio', 'none');
+     svg.style.marginLeft = 0;
+     vbox = svg.getAttribute('viewBox').trim().split(" ");
+     newbox = vbox.join(" ");
+     svg.setAttribute('viewBox', newbox);
+   }
   });
 function animate_paths() {
           var paths = document.querySelectorAll('path');
@@ -45,7 +49,7 @@ function animate_paths() {
           for (i = 0; i < paths.length; ++i) {
             if(paths[i].style['fill'] !="rgb(255, 255, 255)"){
               gr = paths[i].getAttribute('g')*1;
-              li = paths[i].getAttribute('l');
+              li = paths[i].getAttribute('l')*1;
               if(gr >= groups[li]){
                 groups[li] = gr;
               }
@@ -72,7 +76,6 @@ function animate_paths() {
           for(line=0; line <=allLines ; line++ ){
             var ttw = 0;
             for (i = 0; i <=groups[line]; ++i) {
-
               path_group = document.querySelectorAll(`[g="${i}"][l="${line}"]`);
               for (j = 0; j < path_group.length; ++j) {
                 mylength = path_group[j].getTotalLength();
@@ -86,13 +89,50 @@ function animate_paths() {
           window.onscroll =  _.debounce(function() {
             k++;
             if(k==1){
-              animate(TotalLengths);
+              animatefirst(TotalLengths, function(){
+                setTimeout(function(){
+                  w = 0;
+                  window.onscroll =  _.debounce(function() {
+                    w++
+                    if(w==1){
+                      animatesecond(TotalLengths);
+                    }
+                  },0);
+                }, seconds * 1000);
+              });
             }
           }, 100);
     }
 
-    function animate(TotalLengths) {
-      var paths = document.querySelectorAll('path');
+    function animatefirst(TotalLengths, callback) {
+      var mysvg = document.querySelector('#svgpipes1');
+      var paths = mysvg.querySelectorAll('path');
+      for (i = 0; i < paths.length; ++i) {
+          var length = paths[i].getTotalLength();
+          // Set up the starting positions
+          paths[i].style.strokeDasharray = length + ' ' + length;
+          paths[i].style.strokeDashoffset = length;
+          // Trigger a layout so styles are calculated & the browser
+          // picks up the starting position before animating
+          paths[i].getBoundingClientRect();
+          // Define our transition
+          var li = paths[i].getAttribute('l');
+          var total = TotalLengths[li]; // Get TotalLength of current paths, line
+          time_to_excecute = (length/total)*seconds; // paths animation duration
+          paths[i].style['transition-property'] = 'stroke-dashoffset';
+          paths[i].style['transition-duration'] = time_to_excecute + "s";
+          paths[i].style['transition-timing-function'] = transition;
+          // Go!
+          paths[i].style.strokeDashoffset = '0';
+      }
+      if(i == paths.length){
+        callback();
+      }
+    }
+
+    function animatesecond(TotalLengths) {
+      var mysvg = document.querySelector('#svgpipes2');
+      var paths = mysvg.querySelectorAll('path');
       for (i = 0; i < paths.length; ++i) {
           var length = paths[i].getTotalLength();
           // Set up the starting positions
