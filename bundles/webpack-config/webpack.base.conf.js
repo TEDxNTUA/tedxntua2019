617b8c3d5cd8ae26aee142949c0cde8fcd9cb948
Giannis Daras <daras.giannhs@gmail.com>
2019-03-08 22:37:23 +0200

    Update webpack

--- bundles/webpack-config/webpack.base.conf.js
+++ bundles/webpack-config/webpack.base.conf.js
@@ -16,8 +16,8 @@ module.exports = {
     main: fromRoot('assets/js/index.js'),
     home: fromRoot('project/home/assets/js/home.js'),
     pipes: fromRoot('project/home/assets/js/pipes.js'),
-    speakers: fromRoot('project/program/assets/js/speakers.js'),
-    scramble: fromRoot('project/home/assets/js/scramble.js')
+    scramble: fromRoot('project/home/assets/js/scramble.js'),
+    team: fromRoot('project/team/assets/js/team.js')
   },
   module: {
     rules: [{
