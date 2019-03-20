786cdb0f29b774fcbf029f53e977087350399980
Theo Diamantidis <diamaltho@gmail.com>
2019-03-20 02:31:22 +0100

    Added get_image_from_presenter option to Activity admin form

--- project/program/admin.py
+++ project/program/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Presenter, Activity
+from .forms import ActivityModelForm
 from parler.admin import TranslatableAdmin
 
+
+class ActivityAdmin(TranslatableAdmin):
+    form = ActivityModelForm
+
+
 admin.site.register(Presenter, TranslatableAdmin)
-admin.site.register(Activity, TranslatableAdmin)
+admin.site.register(Activity, ActivityAdmin)
